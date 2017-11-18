d2a676e1d5df0387a055d16ab2d5ac5be75c83f2
Sunneva <sunneva_@msn.com>
2017-11-18 15:27:43 +0000

    Basic script

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () { //þeeeetta á kannski að vera hérna, er allavega í verkefni 9 og 8 :)
+
+}
+
+//listi yfir breytur? Það virðist vera gert oft
+
+class videosite {
+
+  /*síðan undir venjulegum kringumstæðum. Sér um að kalla á föll
+  og/eða allt það sem þarf til þess að búa til síðuna áður en eitthvað
+  er leigt af henni */
+  rent() {
+
+  }
+
+
+    /*sér um að byggju upp síðuna, búa til öll elementin sem við þurfum
+     og appenda þau rétt fyrir rent*/
+    createRent() {
+
+    }
+
+    /*nær í gögnin sem við fáum fyrir myndböndin og setur þau inn setur inn
+    svona loading gæja ef það er bið á því að þetta loadist*/
+    load() {
+
+    }
+
+    /*sér um að myndböndin líti rétt út, setur overlay klasa á */
+    VideoApperance() {
+
+    }
+
+    /*útfærir það sem gerist þegar ýtt er á myndbönd*/
+    Watch(){
+
+    }
+
+    /*útfærir controles gæjan, það sem kemur undir þegar
+    maður er að horfa á myndand*/
+    Controles() {
+
+    }
+
+}
